a2df38d76939bf138e9a5a604b3d602cd727095c
Sodeshna Singh <125748274+Sodeshnaa@users.noreply.github.com>
2024-11-06 09:02:35 +0000

    01_variables.js

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "sodeshna"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNum = 77
+
+let stringNumber = String(someNum)
+console.log(stringNumber);
+console.log(typeof stringNumber);
