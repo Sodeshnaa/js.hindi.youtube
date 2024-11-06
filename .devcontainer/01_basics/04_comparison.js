afb4037299aa743860d28dc365080ba3f4efffcc
Sodeshna Singh <125748274+Sodeshnaa@users.noreply.github.com>
2024-11-06 09:52:02 +0000

    04_comparison.js

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,33 @@
+/*majorly used
+console.log(2>1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2>=1);
+console.log(2==1);
+console.log(1!=2);
+*/
+
+/* mostly avoid
+console.log("2" > 1);//true for both
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+*/ 
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
